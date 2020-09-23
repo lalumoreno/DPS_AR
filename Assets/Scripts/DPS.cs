07d9ed19bb63ad86e0bf23d57cc9c815b17f8702
Laura <laamorenoro@unal.edu.co>
2020-09-22 21:18:17 -0500

    app completed

--- Assets/Scripts/DPS.cs
+++ Assets/Scripts/DPS.cs
@@ -76,6 +76,7 @@ public class DPS : MonoBehaviour
             GUI.Label(new Rect(0,0, 200f, 200f), "PELIGRO", styleBig);
             GUI.Label(new Rect(0,0+100f, 200f, 200f), "Desenergize el DPS", styleSmall);
         }
+
     }
 
 
